efa3b767de5b9242f1d961e1b2ed95492ad28221
Martin Forstner <martin@redislabs.com>
2019-12-11 07:34:08 +0100

    fix bug

--- main.py
+++ main.py
@@ -16,11 +16,16 @@ def run(conn, user_id):
     :param user_id: An user ID.
     """
 
+    # each user starts with 100 funds
     conn.hset("user:%s" % user_id, "funds", 100)
-    conn.sadd("inventory:%s" % user_id, *range(1, 10))
+
+    # each user starts with 10 items
+    conn.sadd("user:%s:inventory" % user_id, *range(1, 11))
 
     while True:
-        item_id = random.randint(1, 10)
+
+        # trying to list a random item
+        item_id = conn.srandmember("user:%s:inventory" % user_id)
         price = random.randint(1, 100)
         ret = marketplace.list_item(conn, item_id, user_id, price)
         if ret:
@@ -30,20 +35,28 @@ def run(conn, user_id):
 
         time.sleep(random.randint(1, 10) / 10)
 
+        # trying to purchase a random item
         item_id = random.randint(1, 10)
-        buyer_id = random.randint(1, 10)
+        seller_id = random.randint(1, 10)
         price = random.randint(1, 100)
-        ret = marketplace.purchase_item(conn, buyer_id, item_id, user_id, price)
+        ret = marketplace.purchase_item(conn, user_id, item_id, seller_id, price)
         if ret:
-            print('user:{} purchased item:{} for price {} from user:{}'.format(user_id, item_id, price, buyer_id))
+            print('user:{} purchased item:{} for price {} from user:{}'.format(user_id, item_id, price, seller_id))
         elif ret is False:
-            print('user:{} could NOT purchase item:{} for price {} from user:{}'.format(user_id, item_id, price, buyer_id))
+            print('user:{} could NOT purchase item:{} for price {} from user:{}'.format(user_id, item_id, price, seller_id))
 
         time.sleep(random.randint(1, 10) / 10)
 
 
 if __name__ == '__main__':
+
+    # create a Redis client
     r = redis.StrictRedis(decode_responses=True)
 
-    # spawn 10 greenlets, each simulating an user
-    gevent.joinall([gevent.spawn(run, r, i) for i in range(1, 10)])
+    try:
+
+        # spawn 10 greenlets, each simulating an user
+        gevent.joinall([gevent.spawn(run, r, i) for i in range(1, 11)])
+
+    except KeyboardInterrupt:
+        pass
--- marketplace.py
+++ marketplace.py
@@ -10,31 +10,34 @@ def list_item(conn, item_id, seller_id, price):
     :param item_id: The ID of the item to list.
     :param seller_id: The ID of the user who wants to sell the item.
     :param price: The price to sell.
-    :return: True on success, False otherwise.
+    :return: True on success, None on unmet criteria, False on failure.
     """
-    inventory = "inventory:%s" % seller_id
+    if not item_id:
+        return
+
+    inventory = "user:%s:inventory" % seller_id
     item = "item:%s.%s" % (item_id, seller_id)
     end = time.time() + 5
     pipe = conn.pipeline()
 
+    # try for 5 seconds
     while time.time() < end:
+
         try:
             pipe.watch(inventory)
 
             if not pipe.sismember(inventory, item_id):
                 pipe.unwatch()
-                return None
+                return
 
             pipe.multi()
             pipe.zadd("market", {item: price})
             pipe.srem(inventory, item_id)
-
             pipe.execute()
 
             return True
         except redis.exceptions.WatchError:
-            print('WATCH ERROR for {} in list_item(), retrying ...'.format(inventory))
-            pass
+            print('WATCH ERROR for user:{} in list_item(), retrying ...'.format(seller_id))
 
     return False
 
@@ -48,16 +51,21 @@ def purchase_item(conn, buyer_id, item_id, seller_id, lprice):
     :param item_id: The ID of the item to buy.
     :param seller_id: The ID of the user from who sells the item.
     :param lprice: The max price to buy the item.
-    :return: True on success, False otherwise.
+    :return: True on success, None on unmet criteria, False on failure.
     """
+    if buyer_id == seller_id:
+        return
+
     buyer = "user:%s" % buyer_id
     seller = "user:%s" % seller_id
     item = "item:%s.%s" % (item_id, seller_id)
-    inventory = "inventory:%s" % buyer_id
+    inventory = "uesr:%s:inventory" % buyer_id
     end = time.time() + 10
     pipe = conn.pipeline()
 
+    # try for 10 seconds
     while time.time() < end:
+
         try:
             pipe.watch("market", buyer)
 
@@ -65,11 +73,11 @@ def purchase_item(conn, buyer_id, item_id, seller_id, lprice):
             funds = pipe.hget(buyer, "funds")
             if not price or not funds:
                 pipe.unwatch()
-                return None
+                return
 
             if price > lprice or price > int(funds):
                 pipe.unwatch()
-                return None
+                return
 
             pipe.multi()
             pipe.hincrby(seller, "funds", int(price))
@@ -79,8 +87,7 @@ def purchase_item(conn, buyer_id, item_id, seller_id, lprice):
             pipe.execute()
 
             return True
-        except redis.exceptions.WatchError:
-            print('WATCH ERROR for {} in purchase_item(), retrying ...'.format(buyer))
-            pass
+        except redis.exceptions.WatchError as we:
+            print('WATCH ERROR for user:{} in purchase_item(), retrying ...'.format(buyer_id))
 
     return False
